92bde76666dafd309da537596a224f902ed86883
Neil Michalares <nmichalares@evancedsolutions.com>
2015-08-09 17:06:24 -0400

    Initial Commit

---
+++ michalaresSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(michalaresSite.Startup))]
+namespace michalaresSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
